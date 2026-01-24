9602a884c7ff12d70fd321474633065e884befdc
Shubham soni <shubhamsoni98766@gmai.com>
2026-01-24 17:02:32 +0530

    fix: remove dotenv for Railway production deployment

--- src/main/java/shubham/JobTracker/JobTrackerApplication.java
+++ src/main/java/shubham/JobTracker/JobTrackerApplication.java
@@ -10,9 +10,9 @@ public class  JobTrackerApplication {
 	public static void main(String[] args) {
 
 
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+//		Dotenv dotenv = Dotenv.load();
+//		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+//		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
 
 		SpringApplication.run(JobTrackerApplication.class, args);
 
